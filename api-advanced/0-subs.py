53edf8fcf981ef9aa37612804d6403cb143eff26
lemwaizz <mwaidennis26@gmail.com>
2024-03-02 11:51:05 +0000

    start

---
+++ api-advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+A function that queries the reddit API.
+"""
+
+
+import json
+import requests
+import sys
+
+CLIENT_ID = "l-kWeMxr8KBehyspZ-Yy-w"
+SECRET_KEY = "FlcinD3d6kmC9B0fHOLrrukf8YlQ0Q"
+
+if __name__ == "__main__":
+	def number_of_subscribers(subreddit):
+	    """this function queries the reddit API"""
+
+    	
+  
