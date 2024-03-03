197050ba35e887ce29143d4aa61a1c2b66b13b20
lemwaizz <mwaidennis26@gmail.com>
2024-03-03 21:46:49 +0200

    check again

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,63 +1,37 @@
 #!/usr/bin/python3
-"""A script to retrieve top posts from Reddit API."""
+"""
+Script to print hot posts on a given Reddit subreddit.
+"""
 
-import json
 import requests
-import sys
 
-CLIENT_ID = "l-kWeMxr8KBehyspZ-Yy-w"
-SECRET_KEY = "FlcinD3d6kmC9B0fHOLrrukf8YlQ0Q"
-
-# Request authorization for the Reddit API
-auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRET_KEY)
-
-# Read password from file
-with open('pw.txt', 'r') as f:
-    pw = f.read()
-
-# Initialize dictionary with login information
-data = {
-    'grant_type': 'password',
-    'username': 'LeMwaizz',
-    'password': pw
-}
-
-# Set user agent header
-headers = {'User-Agent': 'apiadvanced/0.0.1'}
-
-response = requests.post('https://www.reddit.com/api/v1/access_token', auth=auth, data=data, headers=headers)
-
-if response.status_code == 200:
-    response_data = response.json()
-    # Check if the response contains an access token
-    if 'access_token' in response_data:
-        token = response_data['access_token']
-        # Manually construct the Authorization header with the token
-        headers['Authorization'] = 'Bearer {}'.format(token)
-    else:
-        print("Failed to obtain access token.")
-        sys.exit(1)
 
 def top_ten(subreddit):
-    """Query the Reddit API to get the titles of the first 10 hot posts for a given subreddit."""
-    url = 'https://oauth.reddit.com/r/{}/hot'.format(subreddit)
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    
-    # Check if the request was successful
-    if response.status_code == 200:
-        subreddit_info = response.json()
-        if 'data' in subreddit_info and 'children' in subreddit_info['data']:
-            # Extract the list of posts
-            posts = subreddit_info['data']['children']
-            # Print the titles of the first 10 posts
-            for post in posts[:10]:
-                print(post['data']['title'])
-    else:
-        print("None")
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    # Construct the URL for the subreddit's hot posts in JSON format
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
 
-# Get subreddit name from command line argument
-if len(sys.argv) > 1:
-    subreddit = sys.argv[1]
-    top_ten(subreddit)
-else:
-    print("Please provide a subreddit name as a command line argument.")
+    # Define headers for the HTTP request, including User-Agent
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+
+    # Define parameters for the request, limiting the number of posts to 10
+    params = {
+        "limit": 10
+    }
+
+    # Send a GET request to the subreddit's hot posts page
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+
+    # Check if the response status code indicates a not-found error (404)
+    if response.status_code == 404:
+        print("None")
+        return
+
+    # Parse the JSON response and extract the 'data' section
+    results = response.json().get("data")
+
+    # Print the titles of the top 10 hottest posts
+    [print(c.get("data").get("title")) for c in results.get("children")]
