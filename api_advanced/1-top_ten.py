c7ff0bc002aa297d1cddc69f42897bc97d0ce3db
lemwaizz <mwaidennis26@gmail.com>
2024-03-03 20:21:35 +0200

    done

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,18 +1,65 @@
 #!/usr/bin/python3
-"""DOCS"""
-import requests
+"""A script to retrieve top posts from Reddit API."""
 
+import json
+import requests
+import sys
+
+CLIENT_ID = "l-kWeMxr8KBehyspZ-Yy-w"
+SECRET_KEY = "FlcinD3d6kmC9B0fHOLrrukf8YlQ0Q"
+
+# Request authorization for the Reddit API
+auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRET_KEY)
+
+# Read password from file
+with open('pw.txt', 'r') as f:
+    pw = f.read()
+
+# Initialize dictionary with login information
+data = {
+    'grant_type': 'password',
+    'username': 'LeMwaizz',
+    'password': pw
+}
+
+# Set user agent header
+headers = {'User-Agent': 'apiadvanced/0.0.1'}
+
+response = requests.post('https://www.reddit.com/api/v1/access_token', auth=auth, data=data, headers=headers)
+
+if response.status_code == 200:
+    response_data = response.json()
+    # Check if the response contains an access token
+    if 'access_token' in response_data:
+        token = response_data['access_token']
+        # Manually construct the Authorization header with the token
+        headers['Authorization'] = 'Bearer {}'.format(token)
+    else:
+        print("Failed to obtain access token.")
+        sys.exit(1)
 
 def top_ten(subreddit):
-    """Docs"""
-    reddit_url = "https://www.reddit.com/r/{}/hot.json" \
-        .format(subreddit)
-    headers = headers = {'User-agent': 'Mozilla/5.0'}
-    response = requests.get(reddit_url, headers=headers)
-
+    """Query the Reddit API to get the titles of the first 10 hot posts for a given subreddit."""
+    url = 'https://oauth.reddit.com/r/{}/hot'.format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    
+    # Check if the request was successful
     if response.status_code == 200:
-        data = response.json()['data']
-        for post in data['children'][:10]:
-            print(post['data']['title'])
+        subreddit_info = response.json()
+        if 'data' in subreddit_info and 'children' in subreddit_info['data']:
+            # Extract the list of posts
+            posts = subreddit_info['data']['children']
+            # Print the titles of the first 10 posts
+            for post in posts[:10]:
+                print(post['data']['title'])
+        else:
+            print("No posts found.")
     else:
-        print(None)
+        print("None")
+
+# Get subreddit name from command line argument
+if len(sys.argv) > 1:
+    subreddit = sys.argv[1]
+    print(top_ten(subreddit))
+else:
+    print("Please provide a subreddit name as a command line argument.")
